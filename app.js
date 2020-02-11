c3441d1f51d23dad91faea79f5c3d41aab86dabb
Sweta Kumari <sweta.shah203@gmail.com>
2020-02-11 08:08:13 +0530

    changed static parameter

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const request = require("request");
 const app=express();
-app.use(express.static(__dirname +"public"));
+app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended:true}));
 app.get("/",function(req,res){
   res.sendFile(__dirname +"/signup.html");
